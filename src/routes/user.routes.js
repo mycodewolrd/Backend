60b98433551c377321236e4ec95ddfe7f523c6cd
Rajnandini <rajnandinipatra2003@gmail.com>
2024-12-02 02:40:01 +0530

    user Logged out

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import {registerUser} from "../controllers/user.controller.js";
+import { registerUser, loginUser, logoutUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 
 const router = Router();
@@ -8,10 +9,10 @@ const router = Router();
 router.route("/register").post(
   //add multer middleware
   upload.fields([
-    // {
-    //   name: "avatar",
-    //   maxCount: 1,
-    // },
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
     {
       name: "coverImage",
       maxCount: 1,
@@ -20,4 +21,9 @@ router.route("/register").post(
   registerUser
 );
 
+router.route("/login").post(loginUser);
+
+
+//secured Routes
+router.route("/logout").post(verifyJWT, logoutUser)
 export default router;
