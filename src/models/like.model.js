76e28f1c7c672eb7256c268380c4564a83524099
Rajnandini <rajnandinipatra2003@gmail.com>
2024-12-06 03:20:52 +0530

    like model

---
+++ src/models/like.model.js
@@ -0,0 +1,35 @@
+import mongoose, { Schema } from "mongoose";
+
+const likeSchema = new Schema(
+    {
+        likedBy: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User"
+            }
+        ],
+        comments: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Comment"
+            }
+        ],
+        video: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        tweet: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Tweet"
+            }
+        ]
+    },
+    {
+        timestamps: true
+    }
+);
+
+export const Like = mongoose.model("Like", likeSchema);
